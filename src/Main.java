184f8a695039c3404766b866eef254f1aa7e9cd5
Toshiba <zhazira9818@gmail.com>
2023-01-11 19:17:34 +0600

    first

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Area area = new Area();
+        area.a = 6;
+        area.b = 7;
+        area.c = 9;
+        area.area();
+
+
+
+    }
+}
